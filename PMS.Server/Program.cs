db3358a435c9414f1ef7c7b49f792d3637b30d15
Preston Taylor <95388976+PrestonLTaylor@users.noreply.github.com>
2024-01-22 18:52:55 +0000

    Server: Add support for Serilog and Seq logging
    We add the support for Seq by using a docker-compose.

--- PMS.Server/Program.cs
+++ PMS.Server/Program.cs
@@ -1,10 +1,18 @@
 using PMS.Server.Services;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddGrpc();
 
+builder.Host.UseSerilog((hostingContext, config) =>
+{
+    config.ReadFrom.Configuration(builder.Configuration);
+});
+
 var app = builder.Build();
+
+app.UseSerilogRequestLogging();
 
 app.MapGrpcService<ProductLookupService>();
 
