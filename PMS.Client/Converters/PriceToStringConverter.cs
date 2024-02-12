94d6bdcc2d7607abc4a81b639759e0ec6148ff09
Preston Taylor <95388976+PrestonLTaylor@users.noreply.github.com>
2024-02-12 16:31:37 +0000

    Client: Implement a price converter for displaying product prices
    We store prices in pennies, however, we want to show the prices in the
    normal format of pounds and pennies.
    
    The PriceToStringConverter is the converter used to turn a price we have
    in a product into a string to be displayed.

---
+++ PMS.Client/Converters/PriceToStringConverter.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+
+namespace PMS.Client.Converters;
+
+public sealed class PriceToStringConverter : IValueConverter
+{
+	object IValueConverter.Convert(object value, Type targetType, object parameter, CultureInfo culture)
+	{
+		if (value is not int price)
+			return "";
+
+		var pounds = price / 100;
+		var pennies = price % 100;
+
+		return $"{pounds}.{pennies:00}";
+	}
+
+	object IValueConverter.ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+	{
+		// TODO: Implement if we need to convert inputted prices by a user
+		throw new NotImplementedException();
+	}
+}
