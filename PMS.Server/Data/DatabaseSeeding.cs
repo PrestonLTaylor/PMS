7f15232e14f0c96b31ebcf0c710174e1c2a40592
Preston Taylor <95388976+PrestonLTaylor@users.noreply.github.com>
2024-03-23 14:19:41 +0000

    feat: Seed a default user into the database for development use
    We create a user with the username and password of "user" and "pass".
    This helps the development process as there is already a user created.

--- PMS.Server/Data/DatabaseSeeding.cs
+++ PMS.Server/Data/DatabaseSeeding.cs
@@ -1,4 +1,6 @@
-﻿using PMS.Server.Models.Fakers;
+﻿using Microsoft.AspNetCore.Identity;
+using PMS.Server.Models;
+using PMS.Server.Models.Fakers;
 
 namespace PMS.Server.Data;
 
@@ -11,7 +13,10 @@ public static class DatabaseSeeding
 		var databaseContext = scope.ServiceProvider.GetRequiredService<DatabaseContext>();
 		await databaseContext.Database.EnsureDeletedAsync();
 		await databaseContext.Database.EnsureCreatedAsync();
+
 		await SeedProductsIntoDatabase(databaseContext);
+		await SeedDefaultUserIntoDatabase(scope);
+
 		await databaseContext.SaveChangesAsync();
 	}
 
@@ -22,5 +27,15 @@ public static class DatabaseSeeding
 		await databaseContext.Products.AddRangeAsync(fakedProducts);
 	}
 
-	private const int NUMBER_OF_FAKES_TO_CREATE = 100;
+    static private async Task SeedDefaultUserIntoDatabase(IServiceScope scope)
+    {
+		const string defaultUsername = "user";
+		const string defaultPassword = "pass";
+
+		var userManager = scope.ServiceProvider.GetRequiredService<UserManager<UserModel>>();
+		var user = new UserModel(defaultUsername);
+		await userManager.CreateAsync(user, defaultPassword);
+    }
+
+    private const int NUMBER_OF_FAKES_TO_CREATE = 100;
 }
