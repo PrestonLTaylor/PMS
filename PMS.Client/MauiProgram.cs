d02482a4dad264768909504375a3591a6ec51abd
Preston Taylor <95388976+PrestonLTaylor@users.noreply.github.com>
2024-03-26 22:53:42 +0000

    feat: Run a service that logs out a user when the login token expires
    For windows, we use HostedServices (manually calling StartAsync) and for
    our other targets we use Shiny (shiny does not have support for Windows).

--- PMS.Client/MauiProgram.cs
+++ PMS.Client/MauiProgram.cs
@@ -1,8 +1,11 @@
 ﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using PMS.Client.Services;
 using PMS.Client.ViewModels;
 using PMS.Client.Views;
 using PMS.Lib;
+using Shiny;
 
 namespace PMS.Client;
 
@@ -13,12 +16,18 @@ public static class MauiProgram
         var builder = MauiApp.CreateBuilder();
         builder
             .UseMauiApp<App>()
+            .UseShiny()
             .ConfigureFonts(fonts =>
             {
                 fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                 fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
             });
 
+#if WINDOWS
+        builder.Services.AddHostedService<LogOutIfTokenHasExpiredJob>();
+#else
+        builder.Services.AddJob(typeof(LogOutIfTokenHasExpiredJob));
+#endif
 
         // TODO: Don't use wait and put this code in an async function somewhere
         SetupConfigurationsAsync(builder.Configuration).Wait();
@@ -38,7 +47,13 @@ public static class MauiProgram
 		builder.Logging.AddDebug();
 #endif
 
-        return builder.Build();
+        var app = builder.Build();
+
+#if WINDOWS
+        StartHostedServices(app.Services);
+#endif
+
+        return app;
     }
 
     private static async Task SetupConfigurationsAsync(ConfigurationManager configuration)
@@ -54,4 +69,15 @@ public static class MauiProgram
         var serverAddress = builder.Configuration["pmsServerAddress"] ?? throw new InvalidOperationException("Unable to find address of pms server");
         builder.Services.AddPMSServices(serverAddress);
     }
+
+#if WINDOWS
+    private static void StartHostedServices(IServiceProvider services)
+    {
+        var hostedServices = services.GetRequiredService<IEnumerable<IHostedService>>();
+        foreach (var service in hostedServices)
+        {
+            service.StartAsync(default).Wait();
+        }
+    }
+#endif
 }
---
+++ PMS.Client/Services/LogOutIfTokenHasExpiredService.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Extensions.Hosting;
+using PMS.Client.Views;
+using PMS.Lib.Services;
+using Shiny.Jobs;
+
+namespace PMS.Client.Services;
+
+public sealed class LogOutIfTokenHasExpiredJob(TokenService _tokenService) 
+    :
+#if WINDOWS
+    IHostedService
+#else
+    IJob
+#endif
+{
+#if WINDOWS
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        _hostedServiceTimer = new Timer((_) => { IfTokenHasExpiredLogOutAsync(); }, null, TimeSpan.Zero, TimeSpan.FromSeconds(10));
+        return Task.CompletedTask;
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+#else
+    public Task Run(JobInfo jobInfo, CancellationToken cancelToken)
+    {
+        IfTokenHasExpiredLogOutAsync();
+        return Task.CompletedTask;
+    }
+#endif
+
+    private void IfTokenHasExpiredLogOutAsync()
+    {
+        if (!_tokenService.HasToken()) return;
+
+        if (_tokenService.HasTokenExpired())
+        {
+            LogOutUser();
+        }
+    }
+
+    private void LogOutUser()
+    {
+        // NOTE: We need to run GoToAsync on the main thread as otherwise we'll cause race-conditions and exceptions
+        MainThread.BeginInvokeOnMainThread(async () =>
+        {
+            await Shell.Current.GoToAsync($"///{nameof(Login)}");
+        });
+    }
+
+#if WINDOWS
+    private Timer _hostedServiceTimer; 
+#endif
+}
